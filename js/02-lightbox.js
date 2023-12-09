d4844743cfb75cf4ec9aa39c5c98d03cca0e2e9e
DurkaMateusz <mosca888@gmail.com>
2023-12-10 00:30:08 +0100

    07 hw
    working

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+document.addEventListener("DOMContentLoaded", function () {
+    const gallery = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+      captionDelay: 250,
+      docClose: true,
+    });
+  });
+const galleryContainer = document.querySelector("ul.gallery");
+const images = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`;
+});
+galleryContainer.insertAdjacentHTML("beforeend", images.join(""));
